459fe01b86f83957f137c1829f85648e7d717529
Adib Mosharrof <adib.mosharrof@gmail.com>
2022-08-09 15:22:02 -0400

    scripts for inference in lcc

--- inference.sh
+++ inference.sh
@@ -1,3 +1,13 @@
-#!/bin/bash
+#! /bin/bash
+
+#SBATCH --time=2:00:00 # Time limit for the job (REQUIRED).
+#SBATCH --job-name=inference # Job name
+#SBATCH --ntasks=8 # Number of cores for the job. Same as SBATCH -n 8
+#SBATCH --partition=V4V32_SKY32M192_L # Partition/queue to run the job in. (REQUIRED)
+#SBATCH -e slurm_out/%j.err # Error file for this job.
+#SBATCH -o slurm_out/%j.out # Output file for this job.
+#SBATCH -A gol_msi290_uksr # Project allocation account name (REQUIRED)
+#SBATCH --gres=gpu:1
+module load ccs/Miniconda3
 source activate ./envs
-time python src/inference.py
+time python src/inference.py --model $1
