9d0874a76cd5be3235505b7ecbb7ca0bef9ee5d4
Adib Mosharrof <adib.mosharrof@gmail.com>
2022-11-29 18:31:16 -0500

    using req slot f1 from SGD eval script

--- src/metrics/requested_slots_metric.py
+++ src/metrics/requested_slots_metric.py
@@ -1,14 +1,17 @@
 import random
+import numpy as np
 from sklearn.metrics import f1_score
 from dstc_dataclasses import DstcRequestedSlot
 from metrics.tod_metrics_base import TodMetricsBase
 from my_enums import SpecialPredictions, SpecialTokens
 from predictions_logger import PredictionLoggerFactory, TodMetricsEnum
 from torchmetrics import F1Score
+from collections import Counter
 
 
 class RequestedSlotsMetric(TodMetricsBase):
-    def __init__(self) -> None:
+    # def __init__(self) -> None:
+    def __init_old__(self) -> None:
         super().__init__()
         # self.all_preds = []
         # self.all_refs = []
@@ -19,7 +22,16 @@ class RequestedSlotsMetric(TodMetricsBase):
         self.add_state("all_preds", [], dist_reduce_fx="cat")
         # self.metrics = [F1Score(average="micro"), F1Score(average="macro")]
 
-    def _update(self, predictions: list[str], references: list[str]) -> any:
+    def __init__(self) -> None:
+        super().__init__()
+        self.prediction_logger = PredictionLoggerFactory.create(
+            TodMetricsEnum.REQUESTED_SLOTS
+        )
+        self.add_state("all_f1", [], dist_reduce_fx="cat")
+        # self.add_state("all_preds", [], dist_reduce_fx="cat")
+        # self.metrics = [F1Score(average="micro"), F1Score(average="macro")]
+
+    def _update_old(self, predictions: list[str], references: list[str]) -> any:
         for ref, pred in zip(references, predictions):
 
             target_txt_items = self._extract_section_and_split_items_from_text(
@@ -38,17 +50,6 @@ class RequestedSlotsMetric(TodMetricsBase):
             )
             pred_slots = [DstcRequestedSlot.from_string(t) for t in pred_txt_items]
 
-            # if len(pred_slots) < len(target_slots):
-            #     diff = len(target_slots) - len(pred_slots)
-            #     pred_slots
-            #     pred_slots.extend(
-            #         [
-            #             DstcRequestedSlot(
-            #                 SpecialPredictions.DUMMY, SpecialPredictions.DUMMY
-            #             )
-            #             for _ in range(diff)
-            #         ]
-            #     )
             if len(pred_slots) == 0 and len(target_slots) == 0:
                 self.all_preds.append(SpecialPredictions.DUMMY)
                 self.all_refs.append(SpecialPredictions.DUMMY)
@@ -69,13 +70,53 @@ class RequestedSlotsMetric(TodMetricsBase):
                     self.all_refs.append(str(slot))
                     self._log_prediction(ref=slot, pred=rand_pred, is_correct=False)
 
-    def _compute(self) -> float:
+    def _compute_old(self) -> float:
         return (
             f1_score(self.all_refs, self.all_preds, average="macro") * 100,
             f1_score(self.all_refs, self.all_preds, average="micro") * 100,
         )
         # return [m(self.all_preds, self.all_refs) for m in self.metrics]
 
-    def __str__(self) -> str:
+    def __str_old__(self) -> str:
         macro_score, micro_score = self.compute()
         return f"Requested Slots Macro, Micro F1:{macro_score:.2f}, {micro_score:.2f}"
+
+    def __str__(self) -> str:
+        score = self.compute()
+        return f"Requested Slots F1:{score:.2f}"
+
+    def _update(self, predictions: list[str], references: list[str]) -> any:
+        for ref, pred in zip(references, predictions):
+
+            target_txt_items = self._extract_section_and_split_items_from_text(
+                ref,
+                SpecialTokens.begin_requested_slots,
+                SpecialTokens.end_requested_slots,
+            )
+            target_slots = [
+                DstcRequestedSlot.from_string(t).slot_name for t in target_txt_items
+            ]
+
+            pred_txt_items = self._extract_section_and_split_items_from_text(
+                pred,
+                SpecialTokens.begin_requested_slots,
+                SpecialTokens.end_requested_slots,
+            )
+            pred_slots = [
+                DstcRequestedSlot.from_string(t).slot_name for t in pred_txt_items
+            ]
+            ref_counter = Counter(target_slots)
+            pred_counter = Counter(pred_slots)
+            true = sum(ref_counter.values())
+            positive = sum(pred_counter.values())
+            true_positive = sum((ref_counter & pred_counter).values())
+            precision = float(true_positive) / positive if positive else 1.0
+            recall = float(true_positive) / true if true else 1.0
+            if precision + recall > 0.0:
+                f1 = 2.0 * precision * recall / (precision + recall)
+            else:  # The F1-score is defined to be 0 if both precision and recall are 0.
+                f1 = 0.0
+            self.all_f1.append(f1)
+
+    def _compute(self) -> float:
+        return np.mean(self.all_f1) * 100
