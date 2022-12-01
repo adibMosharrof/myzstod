b4e930176f7f47007e0914b8a4b96b16d10cc4bd
Adib Mosharrof <adib.mosharrof@gmail.com>
2022-12-01 14:04:57 -0500

    dialog domain exploration added

---
+++ src/data_exploration/dialog_domains.py
@@ -0,0 +1,107 @@
+from collections import defaultdict
+import json
+from multiprocessing import Pool
+import os
+from pathlib import Path
+import sys
+
+from dotmap import DotMap
+import pandas as pd
+from tqdm import tqdm
+
+sys.path.insert(0, os.path.abspath("./src"))
+
+from dstc_dataclasses import DstcDialog, DstcSchema
+from my_enums import Steps
+import utils
+
+# myPath = os.path.dirname(os.path.abspath(__file__))
+# sys.path.insert(0, os.path.abspath(myPath + "/../"))
+
+
+class DialogDomains:
+    def __init__(self, cfg):
+        self.cfg = cfg
+
+    def _read_dialog(self, file):
+        json_dialogs = utils.read_json(file)
+        dstc_dialogs = [DstcDialog.from_dict(js) for js in json_dialogs]
+        dialog_id = file.stem.split("_")[-1]
+        return dialog_id, dstc_dialogs
+
+    def _get_dialogs(self):
+        dialogs_by_steps = {step.value: defaultdict(dict) for step in Steps}
+        for step, num_dialog in zip(Steps, self.cfg.num_dialogs):
+            files = [
+                file
+                for file in (
+                    self.cfg.project_root / self.cfg.raw_data_root / step.value
+                ).iterdir()
+                if not "schema.json" in file.name
+            ][:num_dialog]
+            res = list(
+                tqdm(
+                    Pool().imap(
+                        self._read_dialog,
+                        files,
+                    ),
+                    total=len(files),
+                )
+            )
+            for id, dials in res:
+                dialogs_by_steps[step.value][id] = dials
+        return dialogs_by_steps
+
+    def _group_dialogs_by_domain(self, dialogs_by_steps):
+        domains = {step.value: defaultdict(dict) for step in Steps}
+        for step in Steps:
+            schemas: list[DstcSchema] = [
+                DstcSchema.from_dict(schema_dict)
+                for schema_dict in utils.read_json(
+                    self.cfg.project_root
+                    / self.cfg.raw_data_root
+                    / step.value
+                    / "schema.json"
+                )
+            ]
+
+            for schema in schemas:
+                domains[step.value][schema.service_name] = set()
+            for file_id, dialogs in dialogs_by_steps[step.value].items():
+                for dialog in dialogs:
+                    for service in dialog.services:
+                        domains[step.value][service].add(file_id)
+        return domains
+
+    def print_dialog_domains(self, domains):
+        headers = ["Step", "Domain", "Num Dialogs", "File Ids"]
+        rows = []
+
+        for step in Steps:
+            for domain, file_ids in domains[step.value].items():
+                rows.append(
+                    [step.value, domain, len(file_ids), " ".join(sorted(file_ids))]
+                )
+        file_name = "".join(
+            [self.cfg.out_file_path, "_".join(map(str, self.cfg.num_dialogs)), ".csv"]
+        )
+        utils.write_csv(headers, rows, self.cfg.project_root / file_name)
+
+    def run(self):
+        dialogs = self._get_dialogs()
+        domains = self._group_dialogs_by_domain(dialogs)
+        self.print_dialog_domains(domains)
+        a = 1
+
+
+if __name__ == "__main__":
+    dd = DialogDomains(
+        DotMap(
+            raw_data_root="data/dstc8-schema-guided-dialogue/",
+            processed_data_root="data/processed_data/",
+            project_root=Path("/mounts/u-amo-d0/grad/adibm/projects/generative_tod/"),
+            num_dialogs=[127, 20, 34],
+            out_file_path="data_exploration/dialog_domains",
+        )
+    )
+    dd.run()
