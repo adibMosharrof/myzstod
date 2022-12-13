672c7598687fba34e1e7e74999d4566960ff2f56
Adib Mosharrof <adib.mosharrof@gmail.com>
2022-12-13 12:30:53 -0500

    using env for local rank in sh file

--- trainer.sh
+++ trainer.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 source ~/miniconda3/etc/profile.d/conda.sh
 conda activate ./envs
-outdir=`time python src/trainer.py --config-name simple_tod_trainer_full | tail -n 1`
-var=`time python test.py --out_dir $outdir | tail -n 1`
-echo "$var"
-cd ../google-research
+# outdir=`time python src/trainer.py --config-name simple_tod_trainer_full | tail -n 1`
+# outdir=$(date +'%Y-%m-%d/%H-%M-%S')
+time python -m torch.distributed.launch --use_env --nproc_per_node 1 src/trainer.py --config-name simple_tod_trainer
+# echo $outdir
+# cd ../google-research
 
-source ~/miniconda3/etc/profile.d/conda.sh
-conda activate schema_guided_dst/envs
-python -m schema_guided_dst.evaluate \
---dstc8_data_dir /mounts/u-amo-d0/grad/adibm/projects/generative_tod/data/dstc8-schema-guided-dialogue \
---prediction_dir $outdir/reconstruct/ --eval_set test \
---output_metric_file $outdir/sgd_results.json
+# source ~/miniconda3/etc/profile.d/conda.sh
+# conda activate schema_guided_dst/envs
+# python -m schema_guided_dst.evaluate \
+# --dstc8_data_dir /mounts/u-amo-d0/grad/adibm/projects/generative_tod/data/dstc8-schema-guided-dialogue \
+# --prediction_dir $outdir/reconstruct/ --eval_set test \
+# --output_metric_file $outdir/sgd_results.json
