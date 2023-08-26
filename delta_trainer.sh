14a19e5a0210a7ff08ef2090c412a11ae6516794
Adib Mosharrof <adib.mosharrof@gmail.com>
2023-08-26 12:33:37 -0400

    added arithmetic trainer for delta

--- delta_trainer.sh
+++ delta_trainer.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 
-#SBATCH --time=0-00:10:00 # Time limit for the job (REQUIRED).
+#SBATCH --time=2-00:00:00 # Time limit for the job (REQUIRED).
 #SBATCH --job-name=gt_trainer # Job name
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=16 # Number of cores for the job. Same as SBATCH -n 8
-#SBATCH --partition=gpuA100x4 # Partition/queue to run the job in. (REQUIRED)
+#SBATCH --partition=gpuA40x4 # Partition/queue to run the job in. (REQUIRED)
 #SBATCH -e slurm_out/%j.err # Error file for this job.
 #SBATCH -o slurm_out/%j.out # Output file for this job.
 #SBATCH -A bbyl-delta-gpu # Project allocation account name (REQUIRED)
@@ -13,5 +13,5 @@
 
 source /sw/external/python/anaconda3_gpu/etc/profile.d/conda.sh
 conda  activate ./envs
-which python
-time python src/trainer.py --config-name delta_trainer_lite
+# time python src/trainer.py --config-name delta_trainer_lite
+time python src/trainer.py --config-name delta_arithmetic_trainer
