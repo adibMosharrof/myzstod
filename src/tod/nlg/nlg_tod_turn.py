e5d3a4b265c3443ef34c249399ebbee842d9bfc9
Adib Mosharrof <adib.mosharrof@gmail.com>
2024-03-10 22:36:22 -0400

    is_api_call changed to turn_row_type

--- src/tod/nlg/nlg_tod_turn.py
+++ src/tod/nlg/nlg_tod_turn.py
@@ -4,6 +4,7 @@ from typing import Optional
 from tod.nlg.nlg_tod_context import NlgTodContext
 from tod.nlg.nlg_tod_target import NlgTodTarget
 from sgd_dstc8_data_model.dstc_dataclasses import DstcSchema
+from my_enums import TurnRowType
 
 
 @dataclass
@@ -16,4 +17,4 @@ class NlgTodTurn:
     domains_original: list[str]
     dialog_id: Optional[str] = None
     turn_id: Optional[int] = None
-    is_api_call: Optional[bool] = False
+    turn_row_type: Optional[TurnRowType] = TurnRowType.RESPONSE.value
--- src/tod/turns/api_call_turn_csv_row.py
+++ src/tod/turns/api_call_turn_csv_row.py
@@ -6,12 +6,12 @@ from tod.turns.turn_csv_row_base import TurnCsvRowBase
 class ApiCallTurnCsvRow(TurnCsvRowBase):
     def get_csv_headers(self, should_add_schema: bool = True) -> list[str]:
         headers = super().get_csv_headers(should_add_schema)
-        headers += ["target", "is_api_call"]
+        headers += ["target", "turn_row_type"]
         return headers
 
     def to_csv_row(
         self, context_type: ContextType, tod_turn: NlgTodTurn, should_add_schema: bool
     ) -> list[str]:
         row = super().to_csv_row(context_type, tod_turn, should_add_schema)
-        row.append(int(tod_turn.is_api_call))
+        row.append(int(tod_turn.turn_row_type))
         return row
