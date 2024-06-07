cae6d4410397024cf1df1378530a62187b75fe92
Adib Mosharrof <adib.mosharrof@gmail.com>
2024-06-06 23:06:23 -0400

    gpt context type e2e

--- playground/t5_tod.py
+++ playground/t5_tod.py
@@ -113,7 +113,10 @@ class T5Tod:
         print(self.cfg)
 
     def get_metric_manager(self, context_type: str, tokenizer):
-        if context_type in [ContextType.NLG_API_CALL.value, ContextType.GPT_API_CALL.value]:
+        if context_type in [
+            ContextType.NLG_API_CALL.value,
+            ContextType.GPT_API_CALL.value,
+        ]:
             return NlgApiCallMetricManager(self.logger, tokenizer)
         if context_type == ContextType.KETOD_API_CALL.value:
             return KeTodMetricManager(self.logger, tokenizer)
@@ -360,6 +363,7 @@ class T5Tod:
             if self.cfg.context_type
             in [
                 ContextType.NLG_API_CALL.value,
+                ContextType.GPT_API_CALL.value,
                 ContextType.KETOD_API_CALL.value,
                 ContextType.BITOD.value,
             ]
