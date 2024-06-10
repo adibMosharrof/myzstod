e1c814a3914c5dd3503ffe1657ffdae9a3fa7af9
Adib Mosharrof <adib.mosharrof@gmail.com>
2024-06-10 19:52:37 -0400

    domain categories added to result logger

--- src/logger/results_logger.py
+++ src/logger/results_logger.py
@@ -43,7 +43,10 @@ class ResultsLogger:
 
     def get_regular_setting_results(self, results):
         csv_results = []
-        rows_seen, rows_unseen, rows_mixed = self.get_data_by_settings(results)
+        results = self.get_data_by_settings(results)
+        rows_seen = results[results["domain_category"] == DstcDomains.SEEN.value]
+        rows_unseen = results[results["domain_category"] == DstcDomains.UNSEEN.value]
+        rows_mixed = results[results["domain_category"] == DstcDomains.MIXED.value]
         for rows, setting in zip(
             [results, rows_seen, rows_unseen, rows_mixed],
             ["all", "seen", "unseen", "mixed"],
@@ -56,8 +59,8 @@ class ResultsLogger:
             response_rows = rows[rows["turn_row_type"] == TurnRowType.RESPONSE.value]
             bleu_metric = evaluate.load("bleu")
             gleu_metric = evaluate.load("google_bleu")
-            preds = response_rows.pred
-            refs = np.expand_dims(response_rows.label, axis=1)
+            preds = response_rows.pred.tolist()
+            refs = np.expand_dims(response_rows.label.tolist(), axis=1)
             bleu_score = bleu_metric.compute(predictions=preds, references=refs)["bleu"]
             gleu_score = gleu_metric.compute(predictions=preds, references=refs)[
                 "google_bleu"
@@ -84,6 +87,30 @@ class ResultsLogger:
                         "api_call_param_values",
                         "complete_api_call",
                     ],
+                )
+            )
+            multi_api_rows = api_rows[api_rows["is_multi_domain_api_call"] == 1]
+            multi_api_results = self.get_group_metrics(
+                multi_api_rows,
+                [
+                    "api_call_invoke",
+                    "api_call_method",
+                    "api_call_param_names",
+                    "api_call_param_values",
+                    "complete_api_call",
+                ],
+            )
+            setting_results.update(
+                DotMap(
+                    multi_api_call_invoke=multi_api_results["api_call_invoke"],
+                    multi_api_call_method=multi_api_results["api_call_method"],
+                    multi_api_call_param_names=multi_api_results[
+                        "api_call_param_names"
+                    ],
+                    multi_api_call_param_values=multi_api_results[
+                        "api_call_param_values"
+                    ],
+                    multi_complete_api_call=multi_api_results["complete_api_call"],
                 )
             )
             if "ketod" in self.cfg.raw_data_root.name:
@@ -150,7 +177,7 @@ class ResultsLogger:
         rows_by_domain_setting = {
             step: [] for step in [DstcDomains.SEEN, DstcDomains.UNSEEN]
         }
-        mixed_domain_rows = []
+        domain_categories = []
         for i, row in results.iterrows():
             domains = row["domains"].split(",")
             in_seen_domains = []
@@ -161,13 +188,18 @@ class ResultsLogger:
                 if dom in domain_map[DstcDomains.UNSEEN]:
                     in_unseen_domains.append(1)
             if len(in_seen_domains) > 0 and len(in_unseen_domains) > 0:
-                mixed_domain_rows.append(i)
+                domain_categories.append(DstcDomains.MIXED.value)
+                # mixed_domain_rows.append(i)
             elif len(in_seen_domains) > 0:
-                rows_by_domain_setting[DstcDomains.SEEN].append(i)
+                domain_categories.append(DstcDomains.SEEN.value)
+                # rows_by_domain_setting[DstcDomains.SEEN].append(i)
             elif len(in_unseen_domains) > 0:
-                rows_by_domain_setting[DstcDomains.UNSEEN].append(i)
+                domain_categories.append(DstcDomains.UNSEEN.value)
+                # rows_by_domain_setting[DstcDomains.UNSEEN].append(i)
             else:
                 raise ValueError("no domain match")
+        results["domain_category"] = domain_categories
+        return results
         rows_seen = results.iloc[rows_by_domain_setting[DstcDomains.SEEN]]
         rows_unseen = results.iloc[rows_by_domain_setting[DstcDomains.UNSEEN]]
         rows_mixed = results.iloc[mixed_domain_rows]
@@ -179,6 +211,10 @@ class ResultsLogger:
         # out = {}
         out = {item: {} for item in set(results["domains"])}
         for domain, group in domain_groups:
+            domain_categories = group["domain_category"].unique()
+            domain_categories_text = "|".join(domain_categories)
+            out[domain].update(DotMap(domain_category=domain_categories_text))
+
             bleu = group["response_bleu"].mean().round(4)
             gleu = group["response_gleu"].mean().round(4)
             out[domain].update(DotMap(response_bleu=bleu, response_gleu=gleu))
@@ -277,8 +313,9 @@ class ResultsLogger:
 
     def write_results(self, results, chat_gpt_results):
         col_groups = DotMap(
-            response=["response_bleu", "response_gleu"],
+            response=["domain_category", "response_bleu", "response_gleu"],
             api_call=[
+                "domain_category",
                 "api_call_invoke",
                 "api_call_method",
                 "api_call_param_names",
@@ -286,6 +323,7 @@ class ResultsLogger:
                 "complete_api_call",
             ],
             ke_api_call=[
+                "domain_category",
                 "ke_api_call_invoke",
                 "ke_method",
                 "ke_params",
@@ -293,14 +331,15 @@ class ResultsLogger:
                 "complete_kb_call",
             ],
             multi_api_call=[
+                "domain_category",
                 "multi_api_call_invoke",
                 "multi_api_call_method",
                 "multi_api_call_param_names",
                 "multi_api_call_param_values",
                 "multi_complete_api_call",
             ],
-            retrieval=["retrieval_bleu", "retrieval_gleu"],
-            slot_fill=["slot_fill_bleu", "slot_fill_gleu"],
+            retrieval=["domain_category", "retrieval_bleu", "retrieval_gleu"],
+            slot_fill=["domain_category", "slot_fill_bleu", "slot_fill_gleu"],
         )
         results = sorted(results.items())
         tables = []
@@ -323,9 +362,12 @@ class ResultsLogger:
     def run(self):
         chat_gpt_csv = self.get_csv(self.cfg.chatgpt_results_path)
         results_csv = self.get_csv(self.cfg.results_path)
-        setting_results = self.get_regular_setting_results(results_csv)
-        turn_row_results = self.get_results(results_csv)
-        chat_gpt_results = self.get_results(chat_gpt_csv)
+        results_with_dom_category = self.get_data_by_settings(results_csv)
+        self.get_regular_setting_results(results_with_dom_category)
+        turn_row_results = self.get_results(results_with_dom_category)
+        chat_gpt_with_dom_category = self.get_data_by_settings(chat_gpt_csv)
+        # chat_gpt_results = self.get_results(chat_gpt_csv)
+        chat_gpt_results = self.get_results(chat_gpt_with_dom_category)
         self.write_results(turn_row_results, chat_gpt_results)
         # with open(Path(os.getcwd()) / "results/combined.md", "w") as f:
         #     for table in md_tables:
