1583f6b0749873027a6a93006d0541d3c2c556d0
Adib Mosharrof <adib.mosharrof@gmail.com>
2024-09-25 16:46:07 -0400

    added TodDataset

---
+++ src/datamodules/tod_dataset.py
@@ -0,0 +1,28 @@
+from tod.turns.zs_tod_turn import TodTurnCsvRow
+from torch.utils.data import Dataset
+from pathlib import Path
+
+
+class TodDataSet(Dataset):
+    def __init__(
+        self,
+        data: list[TodTurnCsvRow],
+        dataset_name: str = "",
+        domain_setting: list[str] = None,
+        step_name: str = "",
+        raw_data_root: Path = None,
+    ):
+        self.data: list[TodTurnCsvRow] = data
+        self.dataset_name = dataset_name
+        self.domain_setting = domain_setting
+        self.step_name = step_name
+        self.raw_data_root = raw_data_root
+
+    def get_domain_names(self):
+        return ",".join(self.domain_setting)
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx) -> TodTurnCsvRow:
+        return self.data[idx]
